cedde10c963ddaf9328b4dc2f18c50599875d668
halima precious <halimaprecious3@gmail.com>
2022-05-15 12:10:30 +0300

    ft:add database

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,45 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 
-def create_app():
-    
-    #registering blueprints
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix = "/auth")
+app.config['SECRET_KEY'] = 'no secrets'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+
+def create_app():
+   
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+
+    #initializing flask extensions
+    login_manager.init_app(app)
+    db.init_app(app)
+
+
+
+    #registering blueprints
+    from .auth import auth
+    app.register_blueprint(auth, url_prefix = "/")
+
+    from .views import views
+    app.register_blueprint(views,url_prefix="/")
+
+    create_database(app)
 
 
     return app
+
+def create_database(app):
+    if not path.exists('MY-BLOG' +'blog.db'):
+        db.create_all(app=app)
+        print("Created database!")
